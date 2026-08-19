6e6796722a8b708def93af39f6593f771e8f0dd7
Edrik Kho <khoedrik@gmail.com>
2026-08-19 14:01:24 +0800

    font weight

--- src/Pages/SalesOrders.jsx
+++ src/Pages/SalesOrders.jsx
@@ -1214,7 +1214,7 @@ const SalesOrder = () => {
                       value={salesOrder.date}
                       onChange={handleSalesOrderChange}
                       className={submitted && !salesOrder.date ? "input-error" : ""}
-                      style={{ fontFamily: '"Segoe UI", Tahoma, Geneva, Verdana, sans-serif', fontSize: '14px', fontWeight: 495, color: '#000' }}
+                      style={{ fontFamily: '"Segoe UI", Tahoma, Geneva, Verdana, sans-serif', fontSize: '14px', fontWeight: 498, color: '#000' }}
                     />
                     {submitted && !salesOrder.date && (
                       <span className="error-text">Date is required.</span>
